6d9e3bcf5d83a08faa21b996ff8cb2c8e344e726
Zhang <504947985@qq.com>
2025-11-04 12:45:11 +0800

    修复bugs

--- FileCommands/__main__.py
+++ FileCommands/__main__.py
@@ -1,11 +1,4 @@
-import os
-import sys
-
-
-path = os.path.dirname(__file__)
-sys.path.insert(0, os.path.dirname(path))
-
-from FileCommands import main
+from .main import main
 
 if __name__ == '__main__':
     main()
